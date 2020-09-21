ad86152a72a6c1e3496fc063718b8d9e4fe5b302
aziz mobarak <mobarakaziz9@gmail.com>
2020-09-21 13:19:09 +0100

    bootstrapp+jquery cdn

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import Footer from '../components/footer';
 import Adminlogin from './admin/login';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import Panel from './admin/panel';
 
@@ -71,6 +70,8 @@ if(Component.name==="Panel" || Component.name=="Adminlogin" ){
     <Navbarapp/>
     <Component {...pageProps} />
     <Footer/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </Provider>
     )
 }
